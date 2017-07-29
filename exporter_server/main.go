ec6a2d76819a2abeb95930b6c02f465e2e098b1f
Andrew Ostroumov <andrew.ostroumov@gmail.com>
2017-07-29 18:40:59 +0300

    Rand metrics

--- exporter_server/main.go
+++ exporter_server/main.go
@@ -14,24 +14,60 @@ import (
 var addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
 
 var (
-	requestDuraion = prometheus.NewGauge(
+	gpuTemp0 = prometheus.NewGauge(
 		prometheus.GaugeOpts{
-			Name: "request_durations_seconds",
-			Help: "Request latency distributions.",
+			Name: "gpu_temperature",
+			Help: "GPU Temperature",
+			ConstLabels: map[string]string{ "index": "0", "name": "RX470", "type": "AMD" },
 		},
 	)
+
+	gpuTemp1 = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "gpu_temperature",
+			Help: "GPU Temperature",
+			ConstLabels: map[string]string{ "index": "1", "name": "RX480", "type": "AMD" },
+		},
+	)
+
+	gpuFanSpeed0 = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "gpu_fan_speed",
+			Help: "GPU Fan Speed",
+			ConstLabels: map[string]string{ "index": "0", "name": "RX470", "type": "AMD" },
+		},
+	)
+
+	gpuFanSpeed1 = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "gpu_fan_speed",
+			Help: "GPU Fan Speed",
+			ConstLabels: map[string]string{ "index": "1", "name": "RX480", "type": "AMD" },
+		},
+	)
+
 )
+
+
+func init() {
+	prometheus.MustRegister(gpuTemp0)
+	prometheus.MustRegister(gpuTemp1)
+	prometheus.MustRegister(gpuFanSpeed0)
+	prometheus.MustRegister(gpuFanSpeed1)
+}
 
 func main() {
 	flag.Parse()
 
-	prometheus.MustRegister(requestDuraion)
 
 	go func() {
 		for {
 			v := rand.Float64()
-			requestDuraion.Set(v)
-			time.Sleep(time.Duration(1000) * time.Millisecond)
+			gpuTemp0.Set(v)
+			gpuTemp1.Set(v)
+			gpuFanSpeed0.Set(v)
+			gpuFanSpeed1.Set(v)
+			time.Sleep(time.Duration(10000) * time.Millisecond)
 		}
 	}()
 
