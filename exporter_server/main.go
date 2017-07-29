afff37397708926d321d21f86bf2ccd87ee78628
Andrew Ostroumov <andrew.ostroumov@gmail.com>
2017-07-29 19:18:44 +0300

    Add AMD GPU stats

--- exporter_server/main.go
+++ exporter_server/main.go
@@ -4,9 +4,11 @@ import (
 	"flag"
 	"log"
 	"net/http"
-	"math/rand"
 	"time"
-
+	//"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -62,15 +64,58 @@ func main() {
 
 	go func() {
 		for {
-			v := rand.Float64()
-			gpuTemp0.Set(v)
-			gpuTemp1.Set(v)
-			gpuFanSpeed0.Set(v)
-			gpuFanSpeed1.Set(v)
+			stats := BuildAMDGPUStats()
+			gpuTemp0.Set(stats[0].Temp)
+			gpuTemp1.Set(stats[1].Temp)
+			gpuFanSpeed0.Set(stats[0].FanSpeed)
+			gpuFanSpeed1.Set(stats[1].FanSpeed)
 			time.Sleep(time.Duration(10000) * time.Millisecond)
 		}
 	}()
 
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(*addr, nil))
+}
+
+type AMDGPUStats struct {
+	Temp float64
+	FanSpeed float64
+}
+
+func BuildAMDGPUStats() []AMDGPUStats {
+	tempData0, _ := ioutil.ReadFile("/sys/class/drm/card0/device/hwmon/hwmon0/temp1_input")
+	tempString0 := strings.TrimSpace(string(tempData0))
+	temp0, _ := strconv.ParseFloat(tempString0, 64)
+
+	pwmData0, _ := ioutil.ReadFile("/sys/class/drm/card0/device/hwmon/hwmon0/pwm1")
+	pwmMaxData0, _ := ioutil.ReadFile("/sys/class/drm/card0/device/hwmon/hwmon0/pwm1_max")
+	pwmString0 := strings.TrimSpace(string(pwmData0))
+	pwmMaxString0 := strings.TrimSpace(string(pwmMaxData0))
+	pwm0, _ := strconv.ParseFloat(pwmString0, 64)
+	pwmMax0, _ := strconv.ParseFloat(pwmMaxString0, 64)
+	pwmPercent0 := pwm0 * 100 / pwmMax0
+
+	tempData1, _ := ioutil.ReadFile("/sys/class/drm/card1/device/hwmon/hwmon1/temp1_input")
+	tempString1 := strings.TrimSpace(string(tempData1))
+	temp1, _ := strconv.ParseFloat(tempString1, 64)
+
+	pwmData1, _ := ioutil.ReadFile("/sys/class/drm/card1/device/hwmon/hwmon1/pwm1")
+	pwmMaxData1, _ := ioutil.ReadFile("/sys/class/drm/card1/device/hwmon/hwmon1/pwm1_max")
+	pwmString1 := strings.TrimSpace(string(pwmData1))
+	pwmMaxString1 := strings.TrimSpace(string(pwmMaxData1))
+	pwm1, _ := strconv.ParseFloat(pwmString1, 64)
+	pwmMax1, _ := strconv.ParseFloat(pwmMaxString1, 64)
+	pwmPercent1 := pwm1 * 100 / pwmMax1
+
+
+	return []AMDGPUStats{
+		{
+			Temp: temp0 / 1000,
+			FanSpeed: pwmPercent0,
+		},
+		{
+			Temp: temp1 / 1000,
+			FanSpeed: pwmPercent1,
+		},
+	}
 }
